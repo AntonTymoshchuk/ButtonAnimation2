ae34e7521a24c61707c92c6317950b8e8e579393
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-01 18:27:00 +0300

    Add project files.

---
+++ ButtonAnimation2/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ButtonAnimation2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            button1.TabIndex = 0;
+            button1.TabStop = true;
+            button1.GotFocus += Button1_GotFocus;
+        }
+
+        private void Button1_GotFocus(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
